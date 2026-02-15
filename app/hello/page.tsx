af33c21cb1bd936b7635cc13877e8f9f9a7b815a
NycolasFreitasL <nycolasfretasetec@gmail.com>
2026-02-15 16:27:21 -0300

    aprendendo aos poucos

---
+++ app/hello/page.tsx
@@ -0,0 +1,12 @@
+export default function Hello() {
+  return (
+    <div className="justify-center items-center h-screen flex flex-col bg-linear-to-br from-blue-500 to-purple-500">
+      <h1 className="text-white text-4xl font-bold">hello world</h1>
+      <p className="text-white text-lg">meu nome é nycolas</p>
+      <span>
+        <img src="https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif" alt="gif" />
+          
+      </span>
+    </div>
+  );
+}
